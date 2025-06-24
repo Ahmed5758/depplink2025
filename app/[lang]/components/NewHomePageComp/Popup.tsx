defc997ceecd4741c4ffa59ebdf69f2801c3cc08
Sameer Iqbal <sameeriqbal1200@gmail.com>
2025-06-24 21:10:22 +0500

    changes

--- app/[lang]/components/NewHomePageComp/Popup.tsx
+++ app/[lang]/components/NewHomePageComp/Popup.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Image from 'next/image'
+import Link from 'next/link'
 import React, { useState, Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 
@@ -38,16 +39,18 @@ export default function Popup(props: any, request: any) {
                                 <button type="button" className="focus-visible:outline-none text-white hover:text-white fill-white absolute top-4 right-4" onClick={() => setIsOpen(false)}>
                                     <svg height="16" viewBox="0 0 329.26933 329" width="16" xmlns="http://www.w3.org/2000/svg" id="fi_1828778"><path d="m194.800781 164.769531 128.210938-128.214843c8.34375-8.339844 8.34375-21.824219 0-30.164063-8.339844-8.339844-21.824219-8.339844-30.164063 0l-128.214844 128.214844-128.210937-128.214844c-8.34375-8.339844-21.824219-8.339844-30.164063 0-8.34375 8.339844-8.34375 21.824219 0 30.164063l128.210938 128.214843-128.210938 128.214844c-8.34375 8.339844-8.34375 21.824219 0 30.164063 4.15625 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921875-2.089844 15.082031-6.25l128.210937-128.214844 128.214844 128.214844c4.160156 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921874-2.089844 15.082031-6.25 8.34375-8.339844 8.34375-21.824219 0-30.164063zm0 0"></path></svg>
                                 </button>
-                                <Image
-                                    src="https://images.tamkeenstores.com.sa/assets/new-media/poupformobile.webp"
-                                    alt="popup"
-                                    title="popup-discount"
-                                    height={0}
-                                    width={0}
-                                    sizes='100vw'
-                                    className='rounded-lg h-auto w-full'
-                                    onClick={() => setIsOpen(false)}
-                                />
+                                <Link href={`${props?.lang ? 'ar' : 'en'}/login`} aria-label={`${props?.lang ? 'ar' : 'en'}/login`} target='_blank' onClick={() => setIsOpen(false)}>
+                                    <Image
+                                        src="https://images.tamkeenstores.com.sa/assets/new-media/poupformobile.webp"
+                                        alt="popup"
+                                        title="popup-discount"
+                                        height={0}
+                                        width={0}
+                                        sizes='100vw'
+                                        className='rounded-lg h-auto w-full'
+                                        onClick={() => setIsOpen(false)}
+                                    />
+                                </Link>
                             </Dialog.Panel>
                         </Transition.Child>
                     </div>
