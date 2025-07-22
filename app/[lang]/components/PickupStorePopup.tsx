846fa7c3fcdf84067b1e5297286c10529d0b7c46
Adeel Khan <adeelkhan@techenix.com>
2025-07-22 18:13:46 +0500

    update

--- app/[lang]/components/PickupStorePopup.tsx
+++ app/[lang]/components/PickupStorePopup.tsx
@@ -205,7 +205,7 @@ const PickupStorePopup = (props: any) => {
                                                                 const showroomDirectionText = isArabic ? 'احصل على الاتجاه' : 'Get Direction';
                                                                 const showroomDirectionLink = item?.showroom_data?.direction_button ? item?.showroom_data?.direction_button : '#';
                                                                 return (
-                                                                    <div className={`border ${item?.qty > 0 ? 'border-[#20831E]' : 'border-[#ff5555]'} rounded-md mt-2`} key={i}>
+                                                                    <div className={`border ${item?.qty > 0 ? 'border-[#20831E]' : 'border-primary opacity-45'} rounded-md mt-2`} key={i}>
                                                                         <div className={`cursor-pointer ${item?.qty <= 0 ? 'pointer-events-none' : ''}`}
                                                                             onClick={() => {
                                                                                 setglobalStore(item)
@@ -213,6 +213,7 @@ const PickupStorePopup = (props: any) => {
                                                                                 props?.setModal(false)
                                                                                 topMessageAlartSuccess(isArabic ? 'نجاح! تم تغيير نقطة الالتقاط من المتجر بنجاح..' : 'Success! Pickup From Store Changed Successfully..')
                                                                             }}
+                                                                            aria-disabled = {item?.qty > 0}
                                                                         >
                                                                             <div className='flex justify-between gap-4 items-center p-3'>
                                                                                 <div className='flex items-center gap-3'>
@@ -223,8 +224,8 @@ const PickupStorePopup = (props: any) => {
                                                                                     </div>
                                                                                 </div>
                                                                                 <div className={`${isArabic ? 'w-22' : 'w-20'} flex justify-end items-center`}>
-                                                                                    <div>
-                                                                                        <span className={`border ${item?.qty > 0 ? 'border-[#20831E] text-[#20831E] animationImp' : 'border-[#ff5555] text-[#ff5555]'} py-1 px-3 rounded text-[0.60rem] font-semibold`}>{isArabic ? 'متوفر في المخزون' : 'IN STOCK'}</span>
+                                                                                    <div className='shrink-0'>
+                                                                                        <span className={`border ${item?.qty > 0 ? 'border-[#20831E] text-[#20831E] animationImp' : 'border-primary text-primary'} py-1 px-3 rounded text-[0.60rem] font-semibold`}>{isArabic ? 'متوفر في المخزون' : 'IN STOCK'}</span>
                                                                                         {item?.id == globalStore?.id ?
                                                                                             <div className='flex items-center gap-1 mt-2'>
                                                                                                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0,0,256,256">
