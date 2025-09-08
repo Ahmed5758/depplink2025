6fea4d203efd848d34a924edfe791cd349f7c4e6
Irfan Web <hi0487535@gmail.com>
2025-09-08 13:48:41 +0500

    fix

--- app/[lang]/product/[slug]/page.tsx
+++ app/[lang]/product/[slug]/page.tsx
@@ -1567,10 +1567,10 @@ export default function Product({ params, searchParams }: { params: { lang: stri
                             <div className='flex items-center gap-4 py-0 ltr:pl-2 ltr:pr-4 rtl:pl-4 rtl:pr-2' style={{backgroundColor : badgeBackgroundColor}}>
                                 <Image
                                     src={badgeImageLink}
-                                    width={0} height={0} alt={badgePromoTitle} title={badgePromoTitle} sizes='100vw' className='w-28 h-auto'
+                                    width={0} height={0} alt={badgePromoTitle} title={badgePromoTitle} sizes='100vw' className='w-24 h-auto'
                                 />
                                 <div className='text-sm font-normal'>
-                                    <h6 className='text-base font-extrabold' style={{color:badgeHeadingColor}}>{badgePromoTitle}</h6>
+                                    <h6 className='text-sm font-extrabold' style={{color:badgeHeadingColor}}>{badgePromoTitle}</h6>
                                         <div className="flex items-center gap-1">
                                         <p className="text-xs font-bold">{isArabic ? 'السعر قبل': 'Price Before' }</p>
                                     <span className="font-bold flex items-center gap-1 text-base" style={{color:badgePriceColor}}>{data?.sale_price}{currencyExtraSmallSymbol}</span>
